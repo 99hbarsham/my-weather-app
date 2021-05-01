c03f7e5d655bf3d372c453a7a8a12a8ab4e90e85
99hbarsham <81827110+99hbarsham@users.noreply.github.com>
2021-05-01 11:04:08 +0100

    forecast API integration

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ function currentDesc(response) {
   currentExtras(response)
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -60,6 +61,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML
 }
 
+function getCoords(coordinates) {
+  let apiKey = "7a61ea414ddbfb5e7496aa625238c0b3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function currentTemp(response) {
   let city = document.querySelector("#inputCity1")
   city = response.data.name;
@@ -75,8 +82,9 @@ function currentTemp(response) {
   currentDesc(response)
   
   celsiusTemperature = response.data.main.temp
+
+  getCoords(response.data.coord);
   
-  displayForecast()
 }
 
 function handleSubmit(event) {
@@ -89,6 +97,7 @@ function search(city) {
   let apiKey = "7a61ea414ddbfb5e7496aa625238c0b3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentTemp)
+
 }
 
 function showPosition(position) {
